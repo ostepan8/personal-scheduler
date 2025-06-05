ced154d0e3dab22c3ad65a77df17f73b4c451578
Owen Stepan <106773727+ostepan8@users.noreply.github.com>
2025-06-05 12:10:10 -0400

    Fixing changes

--- api/ApiServer.h
+++ api/ApiServer.h
@@ -1,12 +1,13 @@
 #pragma once
 
 #include "../model/Model.h"
-#include <httplib.h>
+#include "httplib.h"
 
 // ApiServer exposes scheduler functionality via HTTP endpoints.
 // All times in requests/responses are local time strings "YYYY-MM-DD HH:MM".
 // JSON responses use the form {"status":"ok","data":...} or {"status":"error","message":...}
-class ApiServer {
+class ApiServer
+{
 public:
     ApiServer(Model &model, int port = 8080);
     void start();
