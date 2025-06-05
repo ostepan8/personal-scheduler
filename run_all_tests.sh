c26bf7e98a9b23d22833f039f80b433a9784a134
Owen Stepan <106773727+ostepan8@users.noreply.github.com>
2025-06-05 12:49:52 -0500

    Add test runner script and update makefile

---
+++ run_all_tests.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+# Build all test executables
+make recurrence_tests event_tests model_tests model_comprehensive_tests controller_tests
+
+# Run each test executable sequentially
+for t in recurrence_tests event_tests model_tests model_comprehensive_tests controller_tests; do
+    echo "Running $t"
+    ./$t
+    echo
+done
