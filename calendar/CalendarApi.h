4a286603839c33c5a4bb5060a0f27f8985a23d15
Owen Stepan <106773727+ostepan8@users.noreply.github.com>
2025-06-12 18:59:23 -0500

    add update calendar to google api

--- calendar/CalendarApi.h
+++ calendar/CalendarApi.h
@@ -2,9 +2,17 @@
 #include "../model/Event.h"
 #include <memory>
 
-class CalendarApi {
+class CalendarApi
+{
 public:
     virtual ~CalendarApi() = default;
     virtual void addEvent(const Event &e) = 0;
     virtual void deleteEvent(const Event &e) = 0;
+
+    // Helper method to update by delete + add
+    virtual void updateEvent(const Event &oldEvent, const Event &newEvent)
+    {
+        deleteEvent(oldEvent);
+        addEvent(newEvent);
+    }
 };
